2932c564f1df53812bd8d54cf8f2016be48f2493
Vinicius Valverde <viniciusvalverde.tds2.senai@gmail.com>
2024-03-17 20:13:03 -0300

    concat

---
+++ concat.js
@@ -0,0 +1,37 @@
+// concat = Combina, junta (concatena) dois ou mais arrays, retornando um novo array.
+// Não altera nenhuma array, só mescla todas em uma.
+// exemplo 1
+
+//declaro uma array chamada numeroCamisa
+let numeroCamisa =
+       ['5 ', '7 ', '8 ',
+        '10 ', '11 ', '14 ']
+
+// declaro outra array chamada times
+let times =
+    ['boca ', 'juventude ', 'star soccer']
+
+//  declaro uma nova array, concatena, juntando as duas    
+let doisJuntos = numeroCamisa.concat (times); 
+
+// declaro uma mensagem + a nova array 
+let mensageem = "Já usei estes números e joguei por esses times: " + doisJuntos; 
+
+// exibo a mensagem
+console.log (mensageem);
+
+// saída : [ 5 , 7 , 8 , 10 , 11 ,14 ]  e [boca , juventude , star soccer]
+
+// exemplo 2
+// declaro uma array 
+let posicao = 
+    [ 'meia' , 'ponta' , 'atacante' ]
+
+//concateno, junto as 3 arrays
+let tudoJunto = numeroCamisa.concat (times,posicao);
+
+// declaro a mensagem que vou exibir + a nova array
+let mensagem = "Já usei estes numeros, joguei por esses times e nessas posições: " + tudoJunto
+
+console.log(mensagem) // saída: [5 ,7 ,8 ,10 ,11 ,14] [boca ,juventude ,star soccer] [meia,ponta,atacante]
+ 
