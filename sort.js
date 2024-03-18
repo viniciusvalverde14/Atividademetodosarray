d859a0b5d10e111ac47a37bc82ee42bb9f22e3c8
Vinicius Valverde <viniciusvalverde.tds2.senai@gmail.com>
2024-03-17 23:28:20 -0300

    sort

---
+++ sort.js
@@ -0,0 +1,11 @@
+// Ordena os elementos e retorna o array
+
+//exemplo 1
+let letras = [ 'C', 'A', 'B' ] // declaro um array de letras
+letras.sort(); // uso o sort para ordenar
+console.log(letras); // saída :[ 'A', 'B', 'C' ]
+
+//exemplo2
+let numeros = [ 5 , 3 , 2 , 1 , 4]// declaro um array com numeros 
+numeros.sort();// uso o sort para ordenar 
+console.log(numeros);// saída : [ 1, 2, 3, 4, 5 ]
