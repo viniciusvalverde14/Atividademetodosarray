44d480f38bb827e5888c552e278c4f5898d65dfe
Vinicius Valverde <viniciusvalverde.tds2.senai@gmail.com>
2024-03-17 23:40:56 -0300

    lenght

---
+++ lenght.js
@@ -0,0 +1,9 @@
+//retorna o numero de elementos em um array
+
+//exemplo1
+let a = [ 1 , 2 , 3 , 4 ,5 ] // declaro um array com 5 elemntos
+console.log(a.length) // saída : 5
+
+//exemplo2
+let nada = []// declaro um array com nenhum elemento
+console.log(nada.length); //saída: 0
