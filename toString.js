1ef53177e3ca3b9ddbf7194cf3aca3fc67b4b28d
Vinicius Valverde <viniciusvalverde.tds2.senai@gmail.com>
2024-03-17 23:35:07 -0300

    toString

---
+++ toString.js
@@ -0,0 +1,12 @@
+//Retorna uma string representando o array e seus elementos.
+
+//exemplo 1
+let a = [1, 'a' , 'oi' , 10]; // declaro uma array com numeros e strings
+let to = a.toString(); // toString para retornar os elementos em strings 
+console.log(to); // Saída: 1,a,oi,10
+
+//exemplo2
+let b = [10, 20, 30];
+let string = b.toString();
+console.log(string); // Saída: "10,20,30"
+
