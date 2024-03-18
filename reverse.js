063ec57e844f5a2e0f33a31ca58360d2cd6550e1
Vinicius Valverde <viniciusvalverde.tds2.senai@gmail.com>
2024-03-17 23:23:17 -0300

    reverse

---
+++ reverse.js
@@ -0,0 +1,11 @@
+// inverte a ordem dos elementos de um array 
+
+//exemplo 1
+let a = [ 1 , 2 , 3 , 4 ,5 ] // dclaro uma array de numeros
+a.reverse() // uso o reverse para inverter a ordem
+console.log(a) // saída: [ 5, 4, 3, 2, 1 ]
+
+//exemplo 2
+let n = ['gabi' , 'vinicius' ,'pedro'] // declaro uma array com nomes
+n.reverse() // uso o reverse para invertem a ordem
+console.log(n) //saída: [ 'pedro', 'vinicius', 'gabi' ]
